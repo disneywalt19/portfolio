4f6b49f471a96f9d871919becbda8ea70ad4c888
disney19 <disneydj19@gmail.com>
2020-03-14 17:13:33 +0100

    adding css with mix with scss

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,69 +6,19 @@
 
         <title>PORTFOLIO</title>
         
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+        
         <!-- Font Awesome -->
        <script src="https://use.fontawesome.com/releases/v5.12.1/js/all.js" data-mutate-approach="sync"></script>
         
         <!--FontAwesome -->
-        <link href="../../../public/css/app.css" rel="stylesheet">
+<!--        <link href="../../../public/css/app.css" rel="stylesheet">-->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.css">
         
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-image: url("../../public/img/cables.jpg");
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -116,5 +66,6 @@
                 </div>
             </div>
         </div>
+        <script src="{{asset('js/app.js')}}"></script>
     </body>
 </html>
